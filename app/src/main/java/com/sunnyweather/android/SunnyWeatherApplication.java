7adb6829eac68b7b1db1dfdbf8b5eda80f190a45
wyy <3129942459@11.com>
2021-01-09 17:42:13 +0800

    实现全球搜索城市数据功能。。

---
+++ app/src/main/java/com/sunnyweather/android/SunnyWeatherApplication.java
@@ -0,0 +1,16 @@
+package com.sunnyweather.android;
+
+import android.annotation.SuppressLint;
+import android.app.Application;
+import android.content.Context;
+
+public class SunnyWeatherApplication extends Application {
+    public static  final String TOKEN = "EZjHg0y7wx6o0AWO";
+    @SuppressLint("StaticFieldLeak")
+    static public Context context;
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        context =getApplicationContext();
+    }
+}
